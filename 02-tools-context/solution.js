010deba3a3856134454bf236081923655ebb311d
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-16 17:52:34 -0800

    got NaN test to work

--- 02-tools-context/solution.js
+++ 02-tools-context/solution.js
@@ -6,3 +6,13 @@ var first = arr.sort(function (a, b) { return b - a })[0];//get first highest nu
 var second = arr.sort(function (a, b) { return b - a })[1]; //get second highest number
 var highest = [first, second];
 
+function numberCheck() {
+    for (i = 0; i < arr.length; i++) {
+        if (isNaN(arr[i])) {
+            return 'not a number';
+        }
+    }
+    return highest;
+}
+numberCheck();
+
