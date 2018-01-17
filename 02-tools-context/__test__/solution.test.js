1f9d2c9077ada2981bad427980a88bbfc2dc4842
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-16 19:23:36 -0800

    made all prior functions and for loops under one function

---
+++ 02-tools-context/__test__/solution.test.js
@@ -0,0 +1,8 @@
+'use struct';
+
+const arr = require('..lib/solution.js');
+
+describe('Arr Module', function () {
+    describe('#')
+})
+
--- 02-tools-context/lib/solution.js
+++ 02-tools-context/lib/solution.js
@@ -1,45 +1,25 @@
-const arr = [1, 2, 3, 4];
-//const arr = [1, 2, 3, 'l', 4]; testing for NaN
-//const arr = [1, 2, 3, '', 4]; testing for empty string
-
+const arr = [1, 2, 3, 4, 900];
 var first = arr.sort(function (a, b) { return b - a })[0];//get first highest number
 var second = arr.sort(function (a, b) { return b - a })[1]; //get second highest number
 var highest = [first, second];
 
-function numberCheck() {
+function arrCheck() {
     for (i = 0; i < arr.length; i++) {
         if (isNaN(arr[i])) {
             return 'not a number';
         }
     }
-    return highest;
-}
-numberCheck();
-
-//testing for empty string
-function emptyString() {
     for (i = 0; i < arr.length; i++) {
         console.log(arr[i]);
         if (arr[i] === '') {
             return 'contains empty string';
         }
-    } return height;
-}
-emptyString();
-
-//testing for empty array
-function emptyArray() {
+    }
     for (i = 0; i >= arr.length; i++) {
         if (arr.length === 0) {
             return 'contains empty array';
         }
-    } return highest;
-}
-
-emptyArray();
-
-//testing for decimals
-function floatingNumber() {
+    }
     for (i = 0; i < arr.length; i++) {
         if (Number.isInteger(arr[i])) {
             return highest;
@@ -47,6 +27,6 @@ function floatingNumber() {
             return 'not an integer';
         }
     }
+    return highest;
 }
-floatingNumber();
-
+arrCheck();
