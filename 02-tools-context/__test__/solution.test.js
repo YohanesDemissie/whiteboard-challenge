d646da10c93628ad68b08abe7076f8468a8fc528
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-16 20:46:44 -0800

    passed all the tests baby

--- 02-tools-context/__test__/solution.test.js
+++ 02-tools-context/__test__/solution.test.js
@@ -1,20 +1,20 @@
 'use struct';
 
-const array = require('..lib/solution.js');
+const array = require('../lib/solution.js');
 
 describe('Array Module', function () {
     describe('#check', function () {
         it('this should make sure the array is full of numbers', function () {
-            expect(aray.check[4, 77, 'l']).toEqual('not a number')
+            expect(array.check([4, 77, 'l'])).toEqual(null)
         })
         it('this should make sure the array does not contain an empty string', function () {
-            expect(array.check[1, 9, '']).toEqual('contains empty string')
+            expect(array.check([1, 9, ''])).toEqual('contains empty string')
         })
         it('this should make sure the array has some content', function (){
-            expect(array.check[]).toEqual('contains empty array')
+            expect(array.check([])).toEqual('contains empty array')
         })
-        it('this should make sure there are no floating numers', function () {
-            expect(array.check[1, 7, 5.98]).toEqual('not an integer')
+        it('this should make sure it works with floating numbers', function () {
+            expect(array.check([1, 7, 5.98])).toEqual({"first": 7, "second": 5.98})
         })
     })
 })
