1c239e1fa759bd3faa6c4934de7961f7f3541b03
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-17 15:32:50 -0800

    eginner code. Trying to filter NaN

---
+++ whiteboard-challenge-03/solution.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// function const traverse = (engine) => {
+//     console.log()
+   train = { 
+        "value": 2, 
+        "next": {
+            "value": 16, 
+            "next": null
+    }
+}
+
+const traverse = (train) => {
+    for(value=0; value === null; value++)
+        return value.filter(NaN);
+}
