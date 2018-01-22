27cc72578b116a210e3bed4d94fedc3d3ebacf31
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-22 14:02:36 -0800

    created all files/folders. Started loop counter and tests

---
+++ whiteboard-challenge-6/__test__/solution.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const loop = require('../lib/solution.js')
+
+describe('loop module', function () {
+  describe('#counter', function () {
+    it('should return a number', function () {
+     expect(loop([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
+    })
+    it('it should have 2 arguments')
+  })
+})
---
+++ whiteboard-challenge-6/lib/solution.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const loop = module.exports = {};
+
+let loop = function (counter, callback) {
+  if(count === 0)
+  return
+  (count -1, callback)
+}
