e7f1e3321b70c7b74a71c6ae119d516e622ac9a7
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-26 18:43:34 -0800

    start scaffold

---
+++ whiteboard-challenge-10/lib/temp/binary.js
@@ -0,0 +1,40 @@
+num = 3
+arr = [0, 0, 1, 1, 2, 3, 5, 5, 6, 11, 7, 54]
+
+function binarySearch(arr, num, l, r) {
+  if (arr instanceof Array) {
+    l = isNaN(l) ? 0 : l; //left half of binary tree
+    r = isNaN(r) ? arr.length - 1 : r; //right half of binary tree
+    let mid = l + 1 + Math.round((r - l) / 2 - 1); //middle dividin piont
+    // console.log(l, r, mid, arr[mid]);
+    if (num === mid[arr]) {
+      return mid
+    }
+    if (num == arr[mid]) {
+      console.log(arr[mid]);
+      return num;
+    }
+    // test cases
+    if (typeof arr[mid] == "undefined" || l == r) {
+      console.log("not found"); //test case
+      return -1;
+    }
+
+    if (num < arr[mid]) {
+      console.log("take left"); //direction of binarySearch(binary/branch)
+
+      return binarySearch(arr, num, l, r - mid); //return array length - right half
+    }
+
+    console.log("take right"); //or take right, display path
+    return binarySearch(arr, num, mid, r); //return right half of array
+
+  }
+}
+//binarySearch();
+//console.log(binarySearch([0, 0, 1, 1, 2, 3, 5, 5, 6, 11, 7, 54], 1));
+//binarySearch()
+binarySearch(arr, num)
+
+
+//console.log(arr[num], 'shit')
