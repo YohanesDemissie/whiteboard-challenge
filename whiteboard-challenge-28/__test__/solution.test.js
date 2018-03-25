b964b7a5ee1050cae97c5a9f171bd2236ef0a96e
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-03-25 00:05:10 -0700

    PASSING TESTS!!!

--- whiteboard-challenge-28/__test__/solution.test.js
+++ whiteboard-challenge-28/__test__/solution.test.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
-const solution = require('../lib/solution');
+const solution = require('../lib/solution')
 
-describe('Solution Module', () => {
-  describe('highestScore', () => {
+describe('Solution Module', function () {
+  describe('#highWord', function () {
     it('should return null if no arguments passed', function () {
-      expect(solution()).toBe(null); //testing for null if no content
-    });
-    // it('should return null if not a string', function () {
-    //   expect(solution([1, 2, 3])).toEqual(string); //because not string will return null
-    //});
+      expect(solution.highWord()).toEqual(null)
+    })
+    it('should return null if not a string', function () {
+      expect(solution.highWord([1, 2, 3])).toEqual(null)
+    })
     it('should return highest scoring word', function () {
-      expect(solution('aa bb cc dd ee ff gg hh')).toEqual('h');
-    });
-  });
-});
+      expect(solution.highWord('aa bb cc dd ee ff gg')).toEqual('gg')
+    })
+  })
+})
--- whiteboard-challenge-28/lib/solution.js
+++ whiteboard-challenge-28/lib/solution.js
@@ -1,23 +1,23 @@
-'use strict';
+'use strict'
 
-const string = string + '';
-// const words = string.split(' ');
+const solution = module.exports = {}
 
-const solution = module.exports = function (string) {
-  let words = string.split(' ');
-    // let string = string.split(' ') //split by space
-    let max = 0; //counter
-    let highest = ''; //return highest string(word)
-  for (let i = 0; i < words.length; i++) {
-    let s = words[i]; //current
-     let value = 0; //comparing value
-    for (let j = 0; j < s.length; j++) {
-      value += (s.charCodeAt(j) - 96); //pre-written expression that gives letters a numeric value
+solution.highWord = function (string) {
+  if (!string || typeof string !== 'string') return null
+  let currentTotal = 0;
+  let lastTotal = 0;
+  let highWord = '';
+  let strArr = string.split(' ');
+  for (let i = 0; i < strArr.length; i++) {
+    currentTotal = 0;
+    for (let j = 0; j < strArr[i].length; j++) {
+      currentTotal += (strArr[i].charCodeAt(j) - 96);
     }
-    if (value > max) {
-      max = value;
-      highest = s;
+    if (currentTotal > lastTotal) {
+      highWord = strArr[i];
+      lastTotal = currentTotal;
+      currentTotal = 0;
     }
   }
-  return highest;
-};
+  return (highWord);
+}
