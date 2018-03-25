947a53051ec644a5966826b47f91948672f84b10
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-03-24 23:28:13 -0700

    nothing

--- whiteboard-challenge-15/solution.js
+++ whiteboard-challenge-15/solution.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const k_ary = require('./lib/kary-tree.js'); //require in our tree constructor
+const k_ary = require('./lib/k-ary-tree.js'); //require in our tree constructor
 const solution = module.exports = {}; //outsorce our solution file
 
 solution.arr = function (root) { //creating a function iterating through the branches of our root
   let tree = new k_ary();
-  tree = root; 
+  tree = root;
   if (!tree.root) return new Error('Invalid input'); //error first method
   let results = []; //our array that will hold all node values
   let output = function (current) { //the meat and potatoes 
-    if (!current.val.children[0]) results.push(current.val);  
+    if (!current.val.children[0]) results.push(current.val);
   };
   tree.breadthFirst(output);
+  console.log(results, 'results')
   return results;
 };
